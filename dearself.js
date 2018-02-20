f27a7110da436a4b463743e1e7a88c34b2176296
odobok <katrina.kennett@gmail.com>
2018-02-20 10:10:08 -0700

    Create dearself.js

---
+++ dearself.js
@@ -0,0 +1,11 @@
+// good morning / afternoon / evening - pull based on hour
+// pull first name based on blockstack id
+// today's date
+// today's prompt
+// today's quote
+// ability to add prompt / quote 
+// envelope icon if have a message from the past
+// typing box 
+// send to me on <month> <day> <year> 
+// send button 
+// backup with gaia or storage of choice 
